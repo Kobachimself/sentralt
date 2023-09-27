7d2b9c402a5016ac2e9c6ac45a360e344d366e39
YTSentralt <146125239+YTSentralt@users.noreply.github.com>
2023-09-27 21:26:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function toggleSocialMediaIcons() {
     const socialMediaIcons = document.getElementById('social-media-icons');
     socialMediaIcons.style.display = (socialMediaIcons.style.display === 'block') ? 'none' : 'block';
 }
+// Legg til event listeners for klikkfunksjonalitet
+document.querySelectorAll('.social-info').forEach((element) => {
+    element.addEventListener('click', () => {
+        // Hent lenken fra det første ankerelementet i elementet som ble klikket
+        const link = element.querySelector('a').getAttribute('href');
+        // Åpne lenken i ny fane
+        window.open(link, '_blank');
+    });
+});
