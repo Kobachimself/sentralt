3eb2e164ecf794f230d1c2421ffa90d4e47ca206
YTSentralt <146125239+YTSentralt@users.noreply.github.com>
2023-09-27 21:29:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,35 @@ document.querySelectorAll('.social-info').forEach((element) => {
         window.open(link, '_blank');
     });
 });
+// Finn riktig lenke basert på hvilken sosial medieknapp som blir klikket
+function openSocialMediaLink(platform) {
+    let link;
+    switch (platform) {
+        case 'Twitter':
+            link = 'https://twitter.com/ytsentralt';
+            break;
+        case 'Twitch':
+            link = 'https://www.twitch.tv/ytsentralt';
+            break;
+        case 'YouTube':
+            link = 'https://www.youtube.com/@sentralt';
+            break;
+        case 'Facebook':
+            link = 'https://www.facebook.com/profile/ytsentralt';
+            break;
+        case 'Instagram':
+            link = 'https://www.instagram.com/sentralt.07/';
+            break;
+        default:
+            link = '#'; // Default case
+    }
+    window.open(link, '_blank');
+}
+
+// Legg til event listeners for klikkfunksjonalitet
+document.querySelectorAll('.social-info').forEach((element) => {
+    element.addEventListener('click', () => {
+        const platform = element.querySelector('h2').innerText;
+        openSocialMediaLink(platform);
+    });
+});
