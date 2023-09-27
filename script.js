a65e0b7ae574a6ce088b26519b9c8ea8e6307e83
YTSentralt <146125239+YTSentralt@users.noreply.github.com>
2023-09-27 21:09:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleSocialMediaIcons() {
+    const socialMediaIcons = document.getElementById('social-media-icons');
+    socialMediaIcons.style.display = (socialMediaIcons.style.display === 'block') ? 'none' : 'block';
+}
