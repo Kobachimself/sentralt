38cf02eb2f3fcde2dab82b3eb30c66a6da31ae2b
YTSentralt <146125239+YTSentralt@users.noreply.github.com>
2023-10-01 20:22:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -40,6 +40,48 @@ document.querySelectorAll('.social-info').forEach((element) => {
     element.addEventListener('mouseout', () => {
         element.style.backgroundColor = 'transparent';
     });
+// Function to open the correct link based on the platform
+function openLink(platform) {
+    let link;
+    switch (platform) {
+        case 'Twitter':
+            link = 'https://twitter.com/ytsentralt';
+            break;
+        case 'Twitch':
+            link = 'https://www.twitch.tv/ytsentralt';
+            break;
+        case 'YouTube':
+            link = 'https://www.youtube.com/@sentralt';
+            break;
+        case 'Facebook':
+            link = 'https://www.facebook.com/profile/ytsentralt';
+            break;
+        case 'Instagram':
+            link = 'https://www.instagram.com/sentralt.07/';
+            break;
+        case 'PayPal/Donation':
+            link = 'https://paypal.me/sentralt';
+            break;
+        default:
+            link = '#'; // Default case
+    }
+    window.open(link, '_blank');
+}
+
+// Add event listeners to all social media icons
+document.querySelectorAll('.social-info').forEach((element) => {
+    element.addEventListener('click', () => {
+        const platform = element.querySelector('h2').innerText;
+        openLink(platform);
+    });
+
+    element.addEventListener('mouseover', () => {
+        element.style.backgroundColor = 'rgba(66, 134, 244, 0.3)';
+    });
+
+    element.addEventListener('mouseout', () => {
+        element.style.backgroundColor = 'transparent';
+    });
 }); // Function to handle Vipps click
        function openVipps() {
            const vippURL = 'vipps://vipp?amount=0&receiverId=94193690'; // You can adjust the amount if needed
