d8d38a400917006f561c8069a4819b291105f079
YTSentralt <146125239+YTSentralt@users.noreply.github.com>
2023-10-04 16:41:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
 <script>
-    // Function to open the correct link based on the platform
-    function openLink(platform) {
-        let link;
-        switch (platform) {
-            case 'Twitter':
-                link = 'https://twitter.com/ytsentralt';
-                break;
-            case 'Twitch':
-                link = 'https://www.twitch.tv/ytsentralt';
-                break;
-            case 'YouTube':
-                link = 'https://www.youtube.com/channel/UCpawSZMyYJ3T3HR5I8yg33oQ';
-                break;
-            case 'Facebook':
-                link = 'https://www.facebook.com/profile/ytsentralt';
-                break;
-            case 'Instagram':
-                link = 'https://www.instagram.com/sentralt.07/';
-                break;
-            case 'PayPal':
-                link = 'https://paypal.me/sentralt';
-                break;
-            case 'Vipps':
-                const vippURL = 'vipps://vipp?amount=0&receiverId=40886048';
-                window.location.href = vippURL;
-                return; // Stop here to prevent opening a new tab
-            default:
-                link = '#'; // Default case
-        }
-
-        if (platform === 'More') {
-            link = 'socials.html';  // Change this to the desired page
-        }
-
-        window.open(link, '_blank');  // Open in a new tab
+  // Function to open the correct link based on the platform
+  function openLink(platform) {
+    let link;
+    switch (platform) {
+      case 'Twitter':
+        link = 'https://twitter.com/ytsentralt';
+        break;
+      case 'Twitch':
+        link = 'https://www.twitch.tv/ytsentralt';
+        break;
+      case 'YouTube':
+        link = 'https://www.youtube.com/channel/UCpawSZMyYJ3T3HR5I8yg33oQ';
+        break;
+      case 'Facebook':
+        link = 'https://www.facebook.com/profile/ytsentralt';
+        break;
+      case 'Instagram':
+        link = 'https://www.instagram.com/sentralt.07/';
+        break;
+      case 'PayPal':
+        link = 'https://paypal.me/sentralt';
+        break;
+      case 'Vipps':
+        link = 'vipps://vipp?amount=0&receiverId=40886048';
+        window.location.href = link;
+        return; // Stop here to prevent opening a new tab
+      default:
+        link = '#'; // Default case
     }
 
-    // Add event listeners to all social media icons
-    document.querySelectorAll('.social-info').forEach((element) => {
-        element.addEventListener('click', () => {
-            const platform = element.querySelector('h2').innerText;
-            openLink(platform);
-        });
+    if (platform === 'More') {
+      link = 'socials.html';  // Change this to the desired page
+    }
 
-        element.addEventListener('mouseover', () => {
-            element.style.backgroundColor = 'rgba(66, 134, 244, 0.3)';
-        });
+    window.open(link, '_blank');  // Open in a new tab
+  }
 
-        element.addEventListener('mouseout', () => {
-            element.style.backgroundColor = 'transparent';
-        });
+  // Add event listeners to all social media icons
+  document.querySelectorAll('.social-info').forEach((element) => {
+    element.addEventListener('click', () => {
+      const platform = element.querySelector('h2').innerText;
+      openLink(platform);
     });
+
+    element.addEventListener('mouseover', () => {
+      element.style.backgroundColor = 'rgba(66, 134, 244, 0.3)';
+    });
+
+    element.addEventListener('mouseout', () => {
+      element.style.backgroundColor = 'transparent';
+    });
+  });
 </script>
